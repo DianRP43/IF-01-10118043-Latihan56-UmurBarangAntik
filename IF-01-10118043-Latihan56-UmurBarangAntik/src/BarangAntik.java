ad0116c1fec3124c911c8e70e2d63f81eb87d5ec
Dian Rosa Pratama <55683288+DianRP43@users.noreply.github.com>
2019-11-03 10:42:52 +0700

    Add files via upload

---
+++ IF-01-10118043-Latihan56-UmurBarangAntik/src/BarangAntik.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class BarangAntik {
+    private int umur;
+    public BarangAntik (int umur){
+        this.umur = umur;
+    }
+    public void tampilUmur(){
+        System.out.println("Umur barang antik ini adalah " +umur+" tahun");
+    }
+}
